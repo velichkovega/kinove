9b6333dc0401510f109f1885af2818e4c062fa4c
Anatolii Velichko <velichko.vega@gmail.com>
2017-12-20 22:18:51 +0200

    page about add modal

--- src/js/main.js
+++ src/js/main.js
@@ -12,3 +12,19 @@
 $(window).ready(function () {
     console.log('')
 })
+
+$('.persone-img').click(function(e) {
+    e.preventDefault();
+    $('body').css('overflow', 'hidden');
+    $('.overlay').fadeIn(500);
+    $('.custom-modal').removeClass('modal-disable');
+    $('.custom-modal').addClass('modal-animate');
+
+});
+$('.overlay').click(function(event) {
+    // console.log(event);
+    if(event.target !== event.currentTarget) return;
+    $('.overlay').fadeOut(500);
+    $('body').css('overflow-y', 'auto');
+    $('.custom-modal').removeClass('modal-animate').addClass('modal-disable');
+});
